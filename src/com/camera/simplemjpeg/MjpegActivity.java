6aff51b3390096f236333ab3364d68d2733b7d95
golflargeau <langrenay@et.esiea.fr>
2014-03-25 22:22:02 +0100

    initial commit

---
+++ src/com/camera/simplemjpeg/MjpegActivity.java
@@ -0,0 +1,124 @@
+package com.camera.simplemjpeg;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MjpegActivity extends Activity {
+	private static final boolean DEBUG = false;
+	private static final String TAG = "MJPEG";
+
+	private MjpegView mv = null;
+	String URL = "http://82.124.119.123:8080/videofeed";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.main);
+		mv = (MjpegView) findViewById(R.id.mv);
+		new DoRead().execute(URL);
+	}
+
+	@Override
+	public void onResume() {
+		if (DEBUG)
+			Log.d(TAG, "onResume()");
+		super.onResume();
+		if (mv != null) {
+			mv.resumePlayback();
+		}
+
+	}
+
+	@Override
+	public void onStart() {
+		if (DEBUG)
+			Log.d(TAG, "onStart()");
+		super.onStart();
+	}
+
+	@Override
+	public void onPause() {
+		if (DEBUG)
+			Log.d(TAG, "onPause()");
+		super.onPause();
+		if (mv != null) {
+			mv.stopPlayback();
+		}
+	}
+
+	@Override
+	public void onStop() {
+		if (DEBUG)
+			Log.d(TAG, "onStop()");
+		super.onStop();
+	}
+
+	@Override
+	public void onDestroy() {
+		if (DEBUG)
+			Log.d(TAG, "onDestroy()");
+
+		if (mv != null) {
+			mv.freeCameraMemory();
+		}
+
+		super.onDestroy();
+	}
+
+	public class DoRead extends AsyncTask<String, Void, MjpegInputStream> {
+		@Override
+		protected MjpegInputStream doInBackground(String... url) {
+			// TODO: if camera has authentication deal with it and don't just
+			// not work
+			HttpResponse res = null;
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			Log.d(TAG, "1. Sending http request");
+			try {
+				res = httpclient.execute(new HttpGet(URI.create(url[0])));
+				Log.d(TAG, "2. Request finished, status = "
+						+ res.getStatusLine().getStatusCode());
+				if (res.getStatusLine().getStatusCode() == 401) {
+					// You must turn off camera User Access Control before this
+					// will work
+					return null;
+				}
+				return new MjpegInputStream(res.getEntity().getContent());
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+				Log.d(TAG, "Request failed-ClientProtocolException", e);
+				// Error connecting to camera
+			} catch (IOException e) {
+				e.printStackTrace();
+				Log.d(TAG, "Request failed-IOException", e);
+				// Error connecting to camera
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(MjpegInputStream result) {
+			mv.setSource(result);
+			if (result != null)
+				result.setSkip(1);
+			mv.setDisplayMode(MjpegView.SIZE_BEST_FIT);
+			mv.showFps(true);
+		}
+	}
+}
